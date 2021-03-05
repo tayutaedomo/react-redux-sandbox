d247d9d62059b12359e6b73a835c79175b661d5f
tayutaedomo <tayutaedomo@gmail.com>
2021-03-05 19:14:53 +0900

    add: Counter handling

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,27 @@ import React from 'react';
 const App = () => <Counter />;
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMinusButton = (e) => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
-    return <div>counter</div>;
+    return (
+      <>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </>
+    );
   }
 }
 
