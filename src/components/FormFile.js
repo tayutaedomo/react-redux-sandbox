953c35cc7c7cf70d3491ca7a2a58c9e99a174f30
tayutaedomo <tayutaedomo@gmail.com>
2021-03-25 20:40:18 +0900

    fix: FormFile with FieldFileInput component

--- src/components/FormFile.js
+++ src/components/FormFile.js
@@ -2,6 +2,41 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
+// Refer: https://stackoverflow.com/questions/39698285/how-to-upload-file-with-redux-form
+class FieldFileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    const {
+      input: { onChange },
+    } = this.props;
+    //onChange(e.target.files[0]);
+    onChange(e);
+  }
+
+  render() {
+    const {
+      input: { value },
+    } = this.props;
+    const { input, label, required, meta } = this.props; //whatever props you send to the component from redux-form Field
+    return (
+      <div>
+        <label>{label}</label>
+        <div>
+          <input
+            type="file"
+            accept=".jpg, .png, .jpeg"
+            onChange={this.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
 class FormFile extends React.Component {
   onChange(e) {
     console.log('onChange:', e.target.files[0]);
@@ -12,7 +47,7 @@ class FormFile extends React.Component {
       <div>
         <Field
           name="picture"
-          component="input"
+          component={FieldFileInput}
           type="file"
           value={null}
           onChange={this.onChange}
